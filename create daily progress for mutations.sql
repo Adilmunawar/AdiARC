e2f3e318ce327fdc702ecd06701687576cba668c
AdilMunawar <adilfromavengers@gmail.com>
2026-02-07 09:48:31 +0500

    date wise report

--- create daily progress for mutations.sql
+++ create daily progress for mutations.sql
@@ -1,33 +1,31 @@
+DECLARE @ReportDate DATE = '2026-02-06'; -- <== CHANGE THIS DATE AS NEEDED
 SELECT 
+    @ReportDate AS [Report Date],
     U.user_name AS [User Name],
     U.first_name + ' ' + ISNULL(U.last_name, '') AS [Full Name],
-    
     SUM(CASE 
         WHEN I.is_approved = 1 
-             AND CAST(I.intiqal_aprove_date AS DATE) = CAST(GETDATE() AS DATE) 
+             AND CAST(I.intiqal_aprove_date AS DATE) = @ReportDate 
         THEN 1 
         ELSE 0 
-    END) AS [Implemented Today],
-
+    END) AS [Implemented],
     SUM(CASE 
         WHEN (I.is_approved = 0 OR I.is_approved IS NULL) 
-             AND CAST(I.access_datetime AS DATE) = CAST(GETDATE() AS DATE) 
+             AND CAST(I.access_datetime AS DATE) = @ReportDate 
         THEN 1 
         ELSE 0 
-    END) AS [Pending (Active Today)],
-
-    COUNT(I.intiqal_id) AS [Total Activity Today]
-
+    END) AS [Pending (Active)],
+    COUNT(I.intiqal_id) AS [Total Activity]
 FROM 
     [transactions].[Intiqal] I WITH (NOLOCK)
 INNER JOIN 
     [users].[User] U WITH (NOLOCK) ON I.user_id = U.user_id
 WHERE 
-    CAST(I.access_datetime AS DATE) = CAST(GETDATE() AS DATE)
-    OR CAST(I.intiqal_aprove_date AS DATE) = CAST(GETDATE() AS DATE)
+    CAST(I.access_datetime AS DATE) = @ReportDate
+    OR CAST(I.intiqal_aprove_date AS DATE) = @ReportDate
 GROUP BY 
     U.user_name, 
     U.first_name, 
     U.last_name
 ORDER BY 
-    [Implemented Today] DESC;
+    [Implemented] DESC;
--- dataentry report full.sql
+++ dataentry report full.sql
@@ -1,35 +1,31 @@
+DECLARE @ReportDate DATE = '2026-02-06'; -- <== CHANGE THIS DATE AS NEEDED
 WITH 
--- 1. Count Shajra (Family Tree) Entries
 ShajraCounts AS (
     SELECT user_id, COUNT(familytree_id) as cnt 
     FROM [familytree].[FamilyTree] WITH (NOLOCK)
-    WHERE CAST(access_date_time AS DATE) = CAST(GETDATE() AS DATE)
+    WHERE CAST(access_date_time AS DATE) = @ReportDate
     GROUP BY user_id
 ),
--- 2. Count Ownership Entries
 OwnershipCounts AS (
     SELECT user_id, COUNT(ownership_id) as cnt
     FROM [rhz].[Ownership] WITH (NOLOCK)
-    WHERE CAST(access_datetime AS DATE) = CAST(GETDATE() AS DATE)
+    WHERE CAST(access_datetime AS DATE) = @ReportDate
     GROUP BY user_id
 ),
--- 3. Count Khasra Entries
 KhasraCounts AS (
     SELECT user_id, COUNT(khasra_id) as cnt
     FROM [rhz].[Khasra] WITH (NOLOCK)
-    WHERE CAST(access_date_time AS DATE) = CAST(GETDATE() AS DATE)
+    WHERE CAST(access_date_time AS DATE) = @ReportDate
     GROUP BY user_id
 ),
--- 4. Count Possession (Kashtkar) Entries
 PossessionCounts AS (
     SELECT user_id, COUNT(possession_id) as cnt
     FROM [rhz].[Possession] WITH (NOLOCK)
-    WHERE CAST(access_datetime AS DATE) = CAST(GETDATE() AS DATE)
+    WHERE CAST(access_datetime AS DATE) = @ReportDate
     GROUP BY user_id
 )
-
--- 5. Combine All Data
 SELECT 
+    @ReportDate AS [Report Date], 
     sys.fn_varbintohexstr(U.user_name) AS [User Name],
     U.first_name + ' ' + ISNULL(U.last_name, '') AS [Full Name],
     
@@ -38,15 +34,13 @@ SELECT
     ISNULL(K.cnt, 0) AS [Khasra],
     ISNULL(P.cnt, 0) AS [Possession (Kashtkar)],
     
-    (ISNULL(S.cnt, 0) + ISNULL(O.cnt, 0) + ISNULL(K.cnt, 0) + ISNULL(P.cnt, 0)) AS [Total Entries Today]
+    (ISNULL(S.cnt, 0) + ISNULL(O.cnt, 0) + ISNULL(K.cnt, 0) + ISNULL(P.cnt, 0)) AS [Total Entries]
 
 FROM [users].[User] U WITH (NOLOCK)
 LEFT JOIN ShajraCounts S ON U.user_id = S.user_id
 LEFT JOIN OwnershipCounts O ON U.user_id = O.user_id
 LEFT JOIN KhasraCounts K ON U.user_id = K.user_id
 LEFT JOIN PossessionCounts P ON U.user_id = P.user_id
-
--- 6. Filter: Show only users who did at least ONE entry today
 WHERE (ISNULL(S.cnt, 0) + ISNULL(O.cnt, 0) + ISNULL(K.cnt, 0) + ISNULL(P.cnt, 0)) > 0
 
-ORDER BY [Total Entries Today] DESC;
+ORDER BY [Total Entries] DESC;
